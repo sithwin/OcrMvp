b170bd9603dae0883d9e62b365c2d72a03b8a5da
Si Thu Win <Si_Thu_Win@manulife.com>
2019-08-20 11:50:40 +0800

    Initial Mockup

---
+++ OcrMvp/Data/MongoContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcrMvp.Data
+{
+    public class MongoContext
+    {
+        public IMongoDatabase mongoDatabase;
+
+        public MongoContext()
+        {
+            var mongoCleint = new MongoClient("mongodb://localhost:27017");
+            mongoDatabase = mongoCleint.GetDatabase("OcrDB");
+        }
+    }
+}
---
+++ OcrMvp/Models/PolicyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcrMvp.Models
+{
+    public class PolicyInfo
+    {
+        public int Id { get; set; }
+        public string PolicyNumber { get; set; }
+        public string FullName { get; set; }
+        public string IdNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string HomeNumber { get; set; }
+        public string PlanName { get; set; }
+        public decimal SumInsured { get; set; }
+        public decimal TotalPremiumPayable { get; set; }
+        public string PaymentMode { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime InsuredSignedDate { get; set; }
+        public DateTime RepresentativeSignedDate { get; set; }
+        public string RepresentativeName { get; set; }
+        public string RepresentativeCode { get; set; }
+    }
+}
