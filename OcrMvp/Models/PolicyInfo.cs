1f27e05885b54bee858dfa953bec1b20c7bafd81
Si Thu Win <Si_Thu_Win@manulife.com>
2019-08-20 21:17:36 +0800

    Change to MongoDB Bson Object

--- OcrMvp/Models/PolicyInfo.cs
+++ OcrMvp/Models/PolicyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,24 +9,59 @@ namespace OcrMvp.Models
 {
     public class PolicyInfo
     {
-        public int Id { get; set; }
+        [BsonId]
+        public ObjectId Id { get; set; }
         public string PolicyNumber { get; set; }
+
+        [BsonElement]
         public string FullName { get; set; }
+
+        [BsonElement]
         public string IdNumber { get; set; }
-        public DateTime DateOfBirth { get; set; }
+
+        [BsonElement]
+        public String DateOfBirth { get; set; }
+
+        [BsonElement]
         public string Gender { get; set; }
+
+        [BsonElement]
         public string MaritalStatus { get; set; }
+
+        [BsonElement]
         public string Address { get; set; }
+
+        [BsonElement]
         public string Mobile { get; set; }
+
+        [BsonElement]
         public string HomeNumber { get; set; }
+
+        [BsonElement]
         public string PlanName { get; set; }
+
+        [BsonElement]
         public decimal SumInsured { get; set; }
+
+        [BsonElement]
         public decimal TotalPremiumPayable { get; set; }
+
+        [BsonElement]
         public string PaymentMode { get; set; }
+
+        [BsonElement]
         public string PaymentMethod { get; set; }
-        public DateTime InsuredSignedDate { get; set; }
-        public DateTime RepresentativeSignedDate { get; set; }
+
+        [BsonElement]
+        public String InsuredSignedDate { get; set; }
+
+        [BsonElement]
+        public String RepresentativeSignedDate { get; set; }
+
+        [BsonElement]
         public string RepresentativeName { get; set; }
+
+        [BsonElement]
         public string RepresentativeCode { get; set; }
     }
 }
